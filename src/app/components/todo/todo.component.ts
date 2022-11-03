9c3301f63b807a94d500e156110a2758d66c6a80
Nitin K S <nitin@locusnine.com>
2022-11-03 13:41:06 +0530

    enabled localstorage-to store items in ls

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -7,38 +7,36 @@ import { TodoInterface } from './todointerface';
   styleUrls: ['./todo.component.css']
 })
 export class TodoComponent implements OnInit {
-
+  localItem:any;
   todoArr:TodoInterface[]=[]
-  constructor() { }
+  constructor() { 
+    
+  }
 
   ngOnInit(): void {
-    this.todoArr=[
-      {
-        sno:1,
-        title:"jogging",
-        description:"5km",
-        active:true
-  
-      },
-      {
-        sno:2,
-        title:"coding",
-        description:"angular 14",
-        active:true
-      },
-      
-    ]
+    this.localItem=localStorage.getItem("todosElements")
+    console.log("fff",this.localItem)
+    if(this.localItem===null){
+      this.todoArr=[]
+    }
+    else{
+      this.todoArr=JSON.parse(this.localItem)
+    }
   }
 
   deleteTodoelem(item:TodoInterface){
     const index=this.todoArr.indexOf(item)
     this.todoArr.splice(index,1)
     console.log("item",item)
+    localStorage.setItem("todosElements",JSON.stringify(this.todoArr))
  
   }
 
   addTodo(addedtodo:TodoInterface){
-    console.log("addedtodo",addedtodo)
+  
+
+console.log("addedtodo",addedtodo)
 this.todoArr.push(addedtodo)
+localStorage.setItem("todosElements",JSON.stringify(this.todoArr))
   }
 }
